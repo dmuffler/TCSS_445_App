623c3cb7ce9ceed22fb86ee1b855a4b5f253c90f
U-DONALD\donal <donald.muffler@live.com>
2017-11-27 17:44:51 -0800

    Changed sql create to add foreign keys and cascades.

--- SQL_scripts/create.sql
+++ SQL_scripts/create.sql
@@ -1,32 +1,43 @@
+DROP TABLE IF EXISTS Student;
+DROP TABLE IF EXISTS Admin;
+DROP TABLE IF EXISTS Rating;
+DROP TABLE IF EXISTS Instructor;
+
 CREATE TABLE IF NOT EXISTS Student (
   email VARCHAR(50) PRIMARY KEY
-  , password INT
-  , first_name VARCHAR(25)
-  , last_name VARCHAR(25)
-  , gender CHAR)
-;
+  , password BINARY(40) NOT NULL
+  , first_name VARCHAR(25) NOT NULL
+  , last_name VARCHAR(25) NOT NULL
+  , gender CHAR NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS Admin (
     email VARCHAR(50) PRIMARY KEY
-  , password INT
-  , first_name VARCHAR(25)
-  , last_name VARCHAR(25))
-;
+  , password BINARY(40) NOT NULL
+  , first_name VARCHAR(25) NOT NULL
+  , last_name VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Instructor (
+    instructor_id INT PRIMARY KEY
+  , first_name VARCHAR(25) NOT NULL
+  , last_name VARCHAR(25) NOT NULL
+  , gender CHAR NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS Rating (
   instructor_id INT
   , student_email VARCHAR(50)
-  , title VARCHAR(25)
-  , score INT CHECK (score >= 0 AND score <= 5)
-  , hotness INT CHECK (hotness >= 0 AND hotness <= 5)
-  , comment VARCHAR(512)
-  , date DATETIME
+  , title VARCHAR(25) NOT NULL
+  , score INT CHECK (score > 0 AND score <= 5)
+  , hotness INT CHECK (hotness > 0 AND hotness <= 5)
+  , comment VARCHAR(512) NOT NULL
+  , date DATETIME NOT NULL
   , PRIMARY KEY (instructor_id, student_email)
+  , FOREIGN KEY fk_student_email(student_email)
+    REFERENCES Student(email)
+    ON UPDATE CASCADE
+  , FOREIGN KEY fk_instructor_id(instructor_id)
+    REFERENCES Instructor(instructor_id)
+    ON UPDATE CASCADE
 );
-
-CREATE TABLE IF NOT EXISTS Instructor (
-  instructor_id INT PRIMARY KEY
-  , first_name VARCHAR(25)
-  , last_name VARCHAR(25)
-  , gender CHAR
-)
