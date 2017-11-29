49bc104e51d22309618985b8fd4e2d6090496058
Steven Smith <nullspace000@gmail.com>
2017-11-29 09:27:55 -0800

    Change instructor db table schema
    *  Change instructor primary key from int to email
    *  Remove gender column and add phone_number, position_title, and
    department_title
    *  Update API instructor resource to reflect schema changes

--- SQL_scripts/create.sql
+++ SQL_scripts/create.sql
@@ -19,25 +19,27 @@ CREATE TABLE IF NOT EXISTS Admin (
 );
 
 CREATE TABLE IF NOT EXISTS Instructor (
-    instructor_id INT PRIMARY KEY AUTO_INCREMENT
+    email VARCHAR(50) PRIMARY KEY
+  , phone_number VARCHAR(20)
+  , position_title VARCHAR(100)
+  , department_name VARCHAR(150)
   , first_name VARCHAR(25) NOT NULL
   , last_name VARCHAR(25) NOT NULL
-  , gender CHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Rating (
-  instructor_id INT
+  instructor_email VARCHAR(50)
   , student_email VARCHAR(50)
   , title VARCHAR(25) NOT NULL
   , score INT CHECK (score > 0 AND score <= 5)
   , hotness INT CHECK (hotness > 0 AND hotness <= 5)
   , comment VARCHAR(512) NOT NULL
   , date DATETIME NOT NULL
-  , PRIMARY KEY (instructor_id, student_email)
+  , PRIMARY KEY (instructor_email, student_email)
   , FOREIGN KEY fk_student_email(student_email)
     REFERENCES Student(email)
     ON UPDATE CASCADE
-  , FOREIGN KEY fk_instructor_id(instructor_id)
-    REFERENCES Instructor(instructor_id)
+  , FOREIGN KEY fk_instructor_email(instructor_email)
+    REFERENCES Instructor(email)
     ON UPDATE CASCADE
 );
--- php/instructor.php
+++ php/instructor.php
@@ -5,10 +5,20 @@ error_reporting(E_ALL);
 
 require('db.php');
 $sql_statement;
-$create_statement = 'INSERT INTO Instructor (first_name, last_name, gender) VALUES (?, ?, ?)';
-$read_all_statement = 'SELECT instructor_id, first_name, last_name, gender FROM Instructor';
-$update_statement = 'UPDATE Instructor SET first_name = ?, last_name = ?, gender = ? WHERE instructor_id = ?';
-$delete_statement = 'DELETE FROM Instructor WHERE instructor_id = ?';
+$create_statement = 'INSERT INTO Instructor
+                       (email, phone_number,
+                       position_title, department_name,
+                       first_name, last_name)
+                     VALUES (?, ?, ?, ?, ?, ?)';
+$read_all_statement = 'SELECT * FROM Instructor';
+$update_statement = 'UPDATE Instructor SET
+                       first_name = ?,
+                       last_name = ?,
+                       phone_number = ?,
+                       position_title = ?,
+                       department_name = ?
+                     WHERE email = ?';
+$delete_statement = 'DELETE FROM Instructor WHERE email = ?';
 
 /*
  * Returns if the agent is authenticated and an admin, else exits.
@@ -19,7 +29,7 @@ function auth_precheck() {
     if ($sid != '') {
       session_id($sid);
       session_start();
-      if ($_SESSION['is_admin'] == '1') {
+      if (isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == '1') {
         return;
       }
     }
@@ -39,24 +49,36 @@ if ($verb == 'CREATE') {
   $sql_statement = $create_statement;
   $first_name = $_GET['first_name'];
   $last_name = $_GET['last_name'];
-  $gender = $_GET['gender'];
-  $arguments = array($first_name, $last_name, $gender);
+  $position_title = $_GET['position_title'];
+  $department_name = $_GET['department_name'];
+  $phone_number = $_GET['phone_number'];
+  $email = $_GET['email'];
+  $arguments = array($email, $phone_number,
+    $position_title, $department_name,
+    $first_name, $last_name);
 } else if ($verb == 'READ') {
   $sql_statement = $read_all_statement;
   $arguments = array();
 } else if ($verb == 'UPDATE') {
   auth_precheck();
   $sql_statement = $update_statement;
-  $instructor_id = $_GET['id'];
   $first_name = $_GET['first_name'];
   $last_name = $_GET['last_name'];
-  $gender = $_GET['gender'];
-  $arguments = array($first_name, $last_name, $gender, $instructor_id);
+  $position_title = $_GET['position_title'];
+  $department_name = $_GET['department_name'];
+  $phone_number = $_GET['phone_number'];
+  $email = $_GET['email'];
+
+  $arguments = array(
+    $first_name, $last_name,
+    $phone_number,
+    $position_title, $department_name,
+    $email);
 } else if ($verb == 'DELETE') {
   auth_precheck();
   $sql_statement = $delete_statement;
-  $instructor_id = $_GET['id'];
-  $arguments = array($instructor_id);
+  $email = $_GET['email'];
+  $arguments = array($email);
 } else {
   /* unknown verb */
   echo json_encode(array("error" => "Unknown verb"));
